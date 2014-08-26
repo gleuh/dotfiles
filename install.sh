45f2201298b83306f9ae9bf1fb5d8cdb2a66f249
gleuh <lenoirguillaume@gmail.com>
2014-08-26 10:56:53 +0200

    add zsh

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+# submodule
+cd ./dotfiles
+git submodule init
+git submodule update
+cd --
+
 # TMUX
 ln -s ./dotfiles/tmux/tmux.conf ./.tmux.conf
+
+# ZSH
+ln -s ./dotfiles/zsh/zshrc ./.zshrc
+ln -s ./dotfiles/oh-my-zsh ./.oh-my-zsh
+
