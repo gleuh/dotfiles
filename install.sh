9d70753db1c10ecc7997e3a68cd11d4dbb342fdc
Guillaume L <glenoir@lafourchette.com>
2014-08-26 10:57:55 +0200

    add sublime text

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+cd ~
+
 # TMUX
 ln -s ./dotfiles/tmux/tmux.conf ./.tmux.conf
+
+if [ "$1" = "visual" ]
+    then
+    # sublime text
+    ln -s ./dotfiles/sublime-text-3/Installed\ Packages ./Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+    ln -s ./dotfiles/sublime-text-3/Packages ./Library/Application\ Support/Sublime\ Text\ 3/Packages
+fi
